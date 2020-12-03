01a4cf14d2d3a1212b81fdb9397355cecf0f686c
madj14 <aldiialves14@gmail.com>
2020-12-03 23:57:45 +0700

    second commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="count">
+    <v-container>
+      <v-row text="center">
+        <v-col align="center">
+          <h2>⏱️SIMPLE COUNTDOWN⏱️</h2>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols="10" align="center">
+          <v-row>
+            <v-col cols="3"><img src="@/assets/up-arrow.png" width="30px"/></v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><img src="@/assets/up-arrow.png" width="30px"/></v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><img src="@/assets/up-arrow.png" width="30px"/></v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="3"><div>{{ displayHours }}</div></v-col>
+            <v-col cols="1">:</v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><div>{{ displayMinutes }}</div></v-col>
+            <v-col cols="1">:</v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><div>{{ displaySeconds }}</div></v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="3"><img src="@/assets/down-arrow.png" width="30px"/></v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><img src="@/assets/down-arrow.png" width="30px"/></v-col>
+            <v-spacer></v-spacer>
+            <v-col cols="3"><img src="@/assets/down-arrow.png" width="30px"/></v-col>
+          </v-row>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    displayHours: 0,
+    displayMinutes: 0,
+    displaySeconds: 0,
+  }),
+};
+</script>
+
+<style scoped>
+.count{
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="home">
+        <Counter/>
+    </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Counter
+    },
+}
+</script>
+
+<style scoped>
+.home{
+    justify-content: center;
+    display: flex;
+    align-items: center;
+}
+</style>
